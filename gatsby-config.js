0671e38b79982f418f7a39e9d13772f36a965e06
Tommaso Laterza <tommasoltrz@gmail.com>
2021-03-21 19:04:17 +0100

    added netlify cms dependancies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,36 @@
-module.exports = {
-  plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
+export const plugins = [
+  "gatsby-plugin-netlify-cms",
+  "gatsby-plugin-sass",
+  "gatsby-plugin-image",
+  "gatsby-plugin-react-helmet",
+  "gatsby-plugin-sitemap",
+  {
+    resolve: "gatsby-plugin-manifest",
+    options: {
+      icon: "src/images/icon.png",
     },
-    "gatsby-transformer-remark",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-  siteMetadata: {
-    title: "TARALLO",
-    siteUrl: "https://gracious-blackwell-12a72f.netlify.app",
   },
+  "gatsby-transformer-remark",
+  "gatsby-plugin-sharp",
+  "gatsby-transformer-sharp",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "images",
+      path: "./src/images/",
+    },
+    __key: "images",
+  },
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "pages",
+      path: "./src/pages/",
+    },
+    __key: "pages",
+  },
+];
+export const siteMetadata = {
+  title: "TARALLO",
+  siteUrl: "https://gracious-blackwell-12a72f.netlify.app",
 };
