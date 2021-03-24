7e195f6e3790b23a86639c3c76f63d8afb230332
Tommaso Laterza <tommasoltrz@gmail.com>
2021-03-24 22:15:01 +0100

    fix responsiveness

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,7 +8,7 @@ const Hero: React.FC<Props> = ({}) => {
   return (
     <div className={`grid ${styles.heroWrap}`}>
       <div className={`col-12 `}>
-        <h4>Hello there</h4>
+        <h4>Hi there</h4>
         <h1 className={styles.responsiveHeading}>I am Tommaso.</h1>
         <h2 className={styles.responsiveHeading}>I make web things.</h2>
       </div>
